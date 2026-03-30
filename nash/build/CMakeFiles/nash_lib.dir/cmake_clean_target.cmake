553150b7c7b130f858d5494fc3e145297e8fe3ea
Saad Hussain <saadnasir31@gmail.com>
2026-03-30 12:10:53 +0200

    Delete nash/build directory

--- nash/build/CMakeFiles/nash_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libnash_lib.a"
-)
--- nash/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/saad/personal/nash
-# Build directory: /home/saad/personal/nash/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(NashTests "/home/saad/personal/nash/build/nash_tests")
-set_tests_properties(NashTests PROPERTIES  _BACKTRACE_TRIPLES "/home/saad/personal/nash/CMakeLists.txt;55;add_test;/home/saad/personal/nash/CMakeLists.txt;0;")
